956f1d6d3926efd1ccc75c3f7398c1743eb64af5
StevenLong2506 <tatlong2506@gmail.com>
2026-08-31 20:11:52 +0700

    Delete backend directory

--- backend/app/models/place_category.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, ForeignKey
-
-from app.db.base import Base
-
-
-class PlaceCategory(Base):
-    __tablename__ = 'placecategory'
-    category_id = Column(Integer, ForeignKey('category.id', ondelete='CASCADE'),primary_key=True)
-    place_id = Column(Integer, ForeignKey('place.id',ondelete='CASCADE'), primary_key=True)
